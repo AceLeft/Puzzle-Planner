35b7d18ef803478ca148684adc1d605fce2a67c8
AceLeft <brookerwright2@gmail.com>
2022-03-16 19:39:47 -0400

    create testDoLettersMatch()

--- src/test/java/edu/bsu/cs222/testWordGuesser.java
+++ src/test/java/edu/bsu/cs222/testWordGuesser.java
@@ -1,4 +1,18 @@
 package edu.bsu.cs222;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class testWordGuesser {
+    @Test
+    public void testDoLettersMatch(){
+        WordGuesser wordGuesser = new WordGuesser("egg");
+        String kegResult = wordGuesser.doLettersMatch("keg");
+        Assertions.assertEquals("-*g", kegResult);
+        String eggResult = wordGuesser.doLettersMatch("egg");
+        Assertions.assertEquals("egg",eggResult);
+        String topResult = wordGuesser.doLettersMatch("top");
+        Assertions.assertEquals("---",topResult);
+
+    }
 }
